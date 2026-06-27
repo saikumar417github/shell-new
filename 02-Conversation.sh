185f15c6937974dae504a7aa8706a302c6468fd2
saikumar417github <saikumarguggilam@gmail.com>
2026-06-27 13:36:57 +0530

    second file

---
+++ 02-Conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh: Hi Suresh, How are you?"
+echo "Suresh: Hi Ramesh, I am fine, How are you?"
+echo "Ramesh: I am fine Suresh, what else?"
+echo "Suresh: Catch you later Ramesh"
