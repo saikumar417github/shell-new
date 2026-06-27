0881b1ae08a44cbfc2a09b0166e8f1c0caf9d5f4
saikumar417github <saikumarguggilam@gmail.com>
2026-06-27 13:39:27 +0530

    third file

---
+++ 03-Vars-Con.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+$PERSON1=Ramesh
+$PERSON2=Suresh
+
+echo "$PERSON1: Hi $PERSON2, How are you?"
+echo "$PERSON2: Hi $PERSON1, I am fine, How are you?"
+echo "$PERSON1: I am fine $PERSON2, what else?"
+echo "$PERSON2: Catch you later $PERSON1"
